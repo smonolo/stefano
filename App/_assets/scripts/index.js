7e2ec1fa68668df7dbc2a98ace3ba41b94fa537c
Stefano <lu3mtex@gmail.com>
2019-05-11 18:19:14 +0200

    Added error banner and button design changing onclick

--- App/_assets/scripts/index.js
+++ App/_assets/scripts/index.js
@@ -1,20 +1,41 @@
 function randomNumber() {
     const number = $('#number');
     const string = $('#string');
+    const banner = $('#banner');
+    const button = $('#button');
 
     try {
+        button.html('Generating').toggleClass('disabled').attr('disabled', true);
+
         $.ajax({
             url: 'http://localhost:2345/random-number',
             method: 'GET'
         }).done((data) => {
-            number.html(data.number.toString());
-            string.html(data.string);
+            setTimeout(() => {
+                number.html(data.number.toString());
+                string.html(data.string);
+                button.html('Generate').toggleClass('disabled').attr('disabled', false);
+            }, 500);
         }).fail(() => {
-            number.html('0');
-            string.html('A');
+            setTimeout(() => {
+                setTimeout(() => {
+                    banner.fadeOut('slow');
+                }, 2000);
+
+                button.html('Generate').toggleClass('disabled').attr('disabled', false);
+            }, 500);
+
+            banner.html('There was an error, try again.').fadeIn('slow');
         });
     } catch (err) {
-        number.html('0');
-        string.html('A');
+        setTimeout(() => {
+            setTimeout(() => {
+                banner.fadeOut('slow');
+            }, 2000);
+
+            button.html('Generate').toggleClass('disabled').attr('disabled', false);
+        }, 500);
+
+        banner.html('There was an error, try again.').fadeIn('slow');
     }
 }
