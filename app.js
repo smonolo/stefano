566e0b77476891abf2b2438cd127310d1a20fe9f
Stefano <lu3mtex@gmail.com>
2020-01-13 19:58:09 +0100

    Added loading window

--- app.js
+++ app.js
@@ -5,101 +5,33 @@ const richPresence = require('discord-rich-presence')('648198569903390724');
 
 const store = new Store();
 
-let win;
-
-async function window(production) {
-    let timeUsedStart = new Date().getTime();
-
-    log.info(`Starting electron in ${production ? 'production' : 'development'} mode.`);
-    log.debug(`Debug info:\nUsing electron ${process.versions.electron}.` +
-        `\nApp is running on version ${app.getVersion()}.`);
-
-    // Get latest size from config.json
-    let configWidth = store.get('width');
-    let configHeight = store.get('height');
-
-    if (!store.get('timeUsed')) {
-        store.set('timeUsed', 0);
-    }
-
-    // Window default settings
-    win = new BrowserWindow({
-        width: configWidth ? configWidth : 1500,
-        height: configHeight ? configHeight : 1000,
-        minWidth: 600,
-        minHeight: 300,
-        icon: `${__dirname}/images/icon.png`,
-        show: false,
-        title: 'Stefano' + (production ? '' : ' (DEV)')
-    });
-
-    // Remove default menu
-    win.setMenu(null);
-
-    // Automatically open dev tools if app
-    // is in running in development mode
-    if (!production) win.webContents.openDevTools();
-
-    log.info('Reloading and clearing session cache.');
-
-    let loadUrl = production ? 'https://stevyb0t.it' : 'http://localhost:3000';
-
-    // Clear session cache and load url
-    await win.webContents.session.clearCache(() => {
-        win.loadURL(loadUrl).then();
-    });
-
-    // Discord rich presence
-    richPresence.updatePresence({
-        state: 'Visiting stevyb0t.it',
-        startTimestamp: Date.now(),
-        largeImageKey: 'logo',
-        instance: true
-    });
-
-    // Show window once ready
-    win.once('ready-to-show', () => {
-        win.show();
-    });
-
-    // Don't update page title
-    win.on('page-title-updated', (event) => {
-        event.preventDefault();
-    });
-
-    // Don't open new windows
-    win.webContents.on('new-window', (event) => {
-        event.preventDefault();
-    });
-
-    let currentSize = win.getSize();
-
-    win.on('resize', () => {
-        // Get window size on resize event
-        currentSize = win.getSize();
-    });
-
-    win.on('closed', () => {
-        // Get total time in milliseconds
-        let timeUsedFinal = new Date().getTime() - timeUsedStart;
-
-        // Store current width and height for next boot
-        store.set('width', currentSize[0]);
-        store.set('height', currentSize[1]);
-        store.set('timeUsed', store.get('timeUsed') + timeUsedFinal);
-
-        log.debug(`App was used for ${timeUsedFinal} milliseconds.`);
-        log.info(`Changed config size to ${currentSize[0]}, ${currentSize[1]}.`);
-        log.info('Shutting down.');
-
-        // Set window to null, so close it completely
-        win = null;
-    });
-}
+let window, win, loadWin;
 
 let tray = null;
 
 app.on('ready', () => {
+    // Create loading window
+    loadWin = new BrowserWindow({
+        width: 300,
+        height: 300,
+        frame: false,
+        icon: `${__dirname}/images/icon.png`,
+        resizable: false,
+        movable: true,
+        show: false
+    });
+
+    // Remove loading window menu
+    loadWin.setMenu(null);
+
+    // Load loading window url
+    loadWin.loadFile(`${__dirname}/loadWin/index.html`).then();
+
+    // Load once html is loaded
+    loadWin.webContents.on('did-finish-load', () => {
+        loadWin.show();
+    });
+
     // Check if app is running in development mode
     let isProduction = (process.argv || []).indexOf('--dev') === -1;
 
@@ -124,6 +56,7 @@ app.on('ready', () => {
     // Define tray properties
     tray = new Tray(`${__dirname}/images/icon.png`);
 
+    // Create tray icon menu
     let trayMenu = Menu.buildFromTemplate([
         {label: 'Toggle Developer Tools', type: 'normal', click() {win.toggleDevTools()}},
         {type: 'separator'},
@@ -136,7 +69,102 @@ app.on('ready', () => {
     tray.setContextMenu(trayMenu);
 
     // Open window with dev mode boolean
-    window(isProduction).then();
+    window = async function window(production) {
+        let timeUsedStart = new Date().getTime();
+
+        log.info(`Starting electron in ${production ? 'production' : 'development'} mode.`);
+        log.debug(`Debug info:\nUsing electron ${process.versions.electron}.` +
+            `\nApp is running on version ${app.getVersion()}.`);
+
+        // Get latest size from config.json
+        let configWidth = store.get('width');
+        let configHeight = store.get('height');
+
+        if (!store.get('timeUsed')) {
+            store.set('timeUsed', 0);
+        }
+
+        // Window default settings
+        win = new BrowserWindow({
+            width: configWidth ? configWidth : 1500,
+            height: configHeight ? configHeight : 1000,
+            minWidth: 600,
+            minHeight: 300,
+            icon: `${__dirname}/images/icon.png`,
+            show: false,
+            title: 'Stefano' + (production ? '' : ' (DEV)')
+        });
+
+        // Remove default menu
+        win.setMenu(null);
+
+        // Automatically open dev tools if app
+        // is in running in development mode
+        if (!production) win.webContents.openDevTools();
+
+        log.info('Reloading and clearing session cache.');
+
+        let loadUrl = production ? 'https://stevyb0t.it' : 'http://localhost:3000';
+
+        // Clear session cache and load url
+        await win.webContents.session.clearCache(() => {
+            win.loadURL(loadUrl).then();
+        });
+
+        // Discord rich presence
+        richPresence.updatePresence({
+            state: 'Visiting stevyb0t.it',
+            startTimestamp: Date.now(),
+            largeImageKey: 'logo',
+            instance: true
+        });
+
+        // Show window once ready
+        win.once('ready-to-show', () => {
+            // Close loading window
+            loadWin.hide();
+            loadWin.close();
+
+            // Show main window
+            win.show();
+        });
+
+        // Don't update page title
+        win.on('page-title-updated', (event) => {
+            event.preventDefault();
+        });
+
+        // Don't open new windows
+        win.webContents.on('new-window', (event) => {
+            event.preventDefault();
+        });
+
+        let currentSize = win.getSize();
+
+        win.on('resize', () => {
+            // Get window size on resize event
+            currentSize = win.getSize();
+        });
+
+        win.on('closed', () => {
+            // Get total time in milliseconds
+            let timeUsedFinal = new Date().getTime() - timeUsedStart;
+
+            // Store current width and height for next boot
+            store.set('width', currentSize[0]);
+            store.set('height', currentSize[1]);
+            store.set('timeUsed', store.get('timeUsed') + timeUsedFinal);
+
+            log.debug(`App was used for ${timeUsedFinal} milliseconds.`);
+            log.info(`Changed config size to ${currentSize[0]}, ${currentSize[1]}.`);
+            log.info('Shutting down.');
+
+            // Set window to null, so close it completely
+            win = null;
+        });
+    };
+
+    window(isProduction);
 });
 
 // Quit app process once all windows are closed
